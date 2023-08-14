e368bc354d9a0073fc328b6dceb7d952ac05ebe1
Ngwu chukwudum moses <moseschukwudumsnr@gmail.com>
2023-08-14 22:14:48 +0100

    task-3.

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
+const openNav = document.getElementById('open');
+const mobileNav = document.getElementById('sec-nav');
 
-// JavaScript code to show/hide the sign-up form
-const signupButton = document.getElementById('signupButton');
-const signupFormContainer = document.getElementById('signupFormContainer');
 
-signupButton.addEventListener('click', function() {
-    // Toggle the visibility of the sign-up form
-    if (signupFormContainer.style.display === 'none') {
-        signupFormContainer.style.display = 'flex';
-    } else {
-        signupFormContainer.style.display = 'none';
-    }
-});
-     const hamburger = document.querySelector('.hamburger');
-     const wrapper = document.querySelector('.wrapper ul');
+openNav.addEventListener('click', ()=> {
+  openNav.classList.toggle('fa-close');
+  mobileNav.classList.toggle('opened');
+  document.body.classList.toggle('fixed');
+})
 
-        hamburger.addEventListener('click', function() {
-            // Toggle the 'active' class to show/hide the navigation menu
-            nav.classList.toggle('active');
-        });
